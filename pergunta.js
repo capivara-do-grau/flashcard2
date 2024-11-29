f66777433a580bb72689a8fabfb709dbec098c81
capivara-do-grau <160732748+capivara-do-grau@users.noreply.github.com>
2024-11-29 08:12:56 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'fisica',
+    'quantos metros por segundo esta um carro a 180km/h?',
+    'esta a 50m/s.'
+)
+
+criaCartao(
+    'historia',
+    'quando foi a primeira gerra mundial?',
+    'foi em 1914 a 1918.'
+)
+
+criaCartao(
+    'ingles',
+    'qual a tradução de bathroom?',
+    'banheiro'
+)
+
+criaCartao(
+    'Geografia',
+    'QUAL E A CAPITAL DO BRASIL?',
+    'brasilia'
+)
